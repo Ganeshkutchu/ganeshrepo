518ce2e2ead3a51fd43a31ee8d88a77612e90019
Ganeshkutchu <ganeshkitchu777@gmail.com>
2023-02-07 16:56:46 +0530

    first commit

---
+++ Cube.java
@@ -0,0 +1,8 @@
+public class Cube {
+
+    public static void main(String[]args){
+        int l = 93;
+        int result =l*l*l;
+        System.out.println("cube:"+result);
+    }
+}
---
+++ Mycalculator.java
@@ -0,0 +1,10 @@
+public class Mycalculator {
+
+    public static void main(String[]args){
+    int a = 111;
+    int b = 222;
+
+    int result = a+b;
+    System.out.println("the addition two numbers is:"+result);
+    }
+}
